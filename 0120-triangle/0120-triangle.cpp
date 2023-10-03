b10c49015a9ff3d7c5dfdf6d66ad0d7215fe4cef
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2023-10-03 22:57:28 +0530

    Time: 4 ms (75.22%), Space: 8.8 MB (80.74%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -11,11 +11,6 @@ public:
                 else  triangle[i][j] += min(triangle[i-1][j],triangle[i-1][j-1]);         
         }
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                cout<<triangle[i][j]<<" ";
-            }cout<<endl;
-        }
         int ans = INT_MAX;
         for(int i=0;i<triangle[n-1].size();i++){
             ans = min(ans,triangle[n-1][i]);
