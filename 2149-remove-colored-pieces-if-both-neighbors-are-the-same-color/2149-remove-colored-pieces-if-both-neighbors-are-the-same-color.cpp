1bce2da68371f23dd4256452293232f4856abff7
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2023-10-03 18:45:42 +0530

    Time: 32 ms (69.64%), Space: 13.6 MB (17.87%) - LeetHub

--- 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
+++ 2149-remove-colored-pieces-if-both-neighbors-are-the-same-color/2149-remove-colored-pieces-if-both-neighbors-are-the-same-color.cpp
@@ -3,12 +3,12 @@ public:
     bool winnerOfGame(string colors) {
         int a=0,b=0,n=colors.size();
         for(int i=1;i<n-1;i++){
-            if(colors[i]=='A')
+            if(colors[i]=='A'){
             if(colors[i-1]=='A' && colors[i+1]=='A')a++;
-        }
-         for(int i=1;i<n-1;i++){
-            if(colors[i]=='B')
-            if(colors[i-1]=='B' && colors[i+1]=='B')b++;
+            }
+            else{
+                 if(colors[i-1]=='B' && colors[i+1]=='B')b++;
+            }
         }
         return a>b;
     }
