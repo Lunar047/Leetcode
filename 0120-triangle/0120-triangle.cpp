a86d50d91402d9747f55ebbe148afd6b727859c6
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2023-10-03 22:57:19 +0530

    Time: 14 ms (9.4%), Space: 8.9 MB (67.2%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n = triangle.size();
+        for(int i=1;i<n;i++){
+            for(int j=0;j<=i;j++){
+                if(!j){
+                    triangle[i][j] +=triangle[i-1][j];
+                }
+                else if(j==i)  triangle[i][j] +=triangle[i-1][j-1];
+                else  triangle[i][j] += min(triangle[i-1][j],triangle[i-1][j-1]);         
+        }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                cout<<triangle[i][j]<<" ";
+            }cout<<endl;
+        }
+        int ans = INT_MAX;
+        for(int i=0;i<triangle[n-1].size();i++){
+            ans = min(ans,triangle[n-1][i]);
+        }
+        return ans;
+    }
+};
