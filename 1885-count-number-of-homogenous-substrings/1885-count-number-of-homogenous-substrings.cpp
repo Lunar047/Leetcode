f166c4b3533ba2faccdb5742cc21a5de0813eb4e
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2023-10-05 01:50:00 +0530

    Time: 22 ms (79.5%), Space: 12.1 MB (20.95%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 private:
     int mod = 1e9+7;
-    int sumn(long long n){
+    long long sumn(long long n){
         return (n*(n+1)/2)%mod;
     }
 public:
@@ -9,7 +9,8 @@ public:
         if(s.size()==1)return 1;
         else if(s.size()==0)return 0;
         int ans = 0;
-        int left =1,count =1;
+        int left =1;
+        long long count =1;
         char c= s[0];
         while(left<s.size()){
             if(s[left]!=c){
