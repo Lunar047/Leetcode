49e4b7ad3b6c8e27201f4a6b99dc337e2681d945
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-07-07 23:36:44 +0530

    Time: 0 ms (100%), Space: 8.2 MB (93.11%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
-    void solve(vector<int> nums, vector<int> output,int index, vector<vector<int>> &ans) 
-    {
-       //base case
-       if(index>=nums.size())
-       {
-           ans.push_back(output);
-           return ;
-       }
-       //exclude case
-        solve(nums,output,index+1,ans);
-       //include case
-       output.push_back(nums[index]);
-        solve(nums,output,index+1,ans);
-       
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        int index =0;
-        solve(nums,output,index,ans);
-        return ans;
+        int n = nums.size();
+        int k = 1<<n;
+        vector<vector<int>> powerSet(k);
+        for(int i=0;i<(1<<n);i++){
+            for(int j=0;j<n;j++){
+                if(i &(1<<j)){
+                    powerSet[i].push_back(nums[j]);
+                }
+            }
+        }
+        return powerSet;
     }
 };
