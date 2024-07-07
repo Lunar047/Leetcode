16b38e83bac95d337b917329306ecea6284de6e4
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-07-07 20:59:05 +0530

    Time: 4 ms (15.63%), Space: 7.9 MB (86.86%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,51 +1,25 @@
 class Solution {
-public:
-    string s[10] = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    void solve(vector<string> & ans,string c,int in,string &dig){
-        if(in>=dig.size()){
-            ans.push_back(c);
+  private:
+    void solve(int i,vector<string>&ans,string &a,vector<string>&phone,string &cur,int &N){
+        if(i>=N){
+            if(cur.size())ans.push_back(cur);
             return;
         }
-        int t = dig[in]-'0';
-        for(int i=0;i<s[t].size();i++){
-            c.push_back(s[t][i]);
-            solve(ans,c,in+1,dig);
-            c.pop_back();
+        // cout<<a[i]-'0'<<" ";
+        for(int j=0;j<phone[a[i]-'0'].size();j++){
+            cur.push_back(phone[a[i]-'0'][j]);
+            solve(i+1,ans,a,phone,cur,N);
+            cur.pop_back();
         }
+        
     }
+public:
     vector<string> letterCombinations(string digits) {
-        vector<string> ans;
-        if(digits.size()<1)return ans;
-        string c;
-        int i =0;
-        solve(ans,c,i,digits);
+         vector<string> ans;
+        vector<string> phone = { "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string cur;
+        int n = digits.size();
+        solve(0,ans,digits,phone,cur,n);
         return ans;
     }
-    // void solve(string d,string o,int i,vector<string> &ans)
-    // {
-    //    if(i>=d.size())
-    //    {
-    //        ans.push_back(o);
-    //        return ;
-    //    }
-    //    int j = 0,in=d[i]-'0';
-    //    string s2 = s[in];
-    //    while(j<s2.length())
-    //    {
-    //        o.push_back(s2[j]);
-    //        solve(d,o,i+1,ans);
-    //        o.pop_back();
-    //        j++;
-    //    }
-
-    // }
-    // vector<string> letterCombinations(string digits) {
-    //  vector<string> ans;
-    //  string o;
-    //  int i=0;
-    //  if(digits.size()<1)
-    //  return ans;
-    //  solve(digits,o,i,ans);
-    //  return ans;
-    // }
 };
