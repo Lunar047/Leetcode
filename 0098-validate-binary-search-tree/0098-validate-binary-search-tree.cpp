1bd307f965e56c704b3d170dbba96bd709c818ac
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-07-09 23:39:39 +0530

    Time: 12 ms (22.38%), Space: 20 MB (99.19%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,34 +9,17 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-typedef long long ll;
 class Solution {
 private:
-    bool isBST(TreeNode* root,ll low = LLONG_MIN,ll high = LLONG_MAX){
+    typedef long long ll;
+    bool isBST(TreeNode* root,ll left,ll right){
         if(!root)return 1;
-        else if(( root->val<=low) || ( root->val>=high))return 0;
-        else return isBST(root->left,low,root->val) && isBST(root->right,root->val,high);
+        if(root->val>left && root->val<right)return isBST(root->left,left,root->val) && isBST(root->right,root->val,right);;
+        return 0;
     }
 public:
     bool isValidBST(TreeNode* root) {
         if(!root || (!root->left && !root->right))return 1;
-        return isBST(root);
+        return isBST(root,LLONG_MIN,LLONG_MAX);
     }
-    // bool isValidBST(TreeNode* root) {
-    //     if(!root || (!root->left && !root->right))return 1;
-    //     else if(!root->left || !root->right){
-    //         if(!root->left){
-    //             if(root->right->val<=root->val)return 0;
-    //             else return isValidBST(root->right);
-    //         }
-    //         else{
-    //             if(root->left->val>=root->val)return 0;
-    //             else return isValidBST(root->left);
-    //         }
-    //     }
-    //     else{
-    //         if(root->left->val>=root->val || root->right->val<=root->val)return 0;
-    //         else return isValidBST(root->left) && isValidBST(root->right);
-    //     }
-    // }
 };
