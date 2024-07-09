f28a1bd11fb0133f132038197174601bdb44acfd
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-07-09 23:46:25 +0530

    Time: 0 ms (100%), Space: 7.3 MB (32.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,27 +1,11 @@
 class Solution {
 public:
-    int solve(int n,vector<int> &dp){
-        if(n==0)return 1;
-        else if(n<0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n] = solve(n-1,dp)+solve(n-2,dp);
-    }
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        dp[0] = 1;
-        return solve(n,dp);
-
+        vector<int> dp(n+1,0);
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
-
-//  // This recursion method works perfectly for n = 40 and we need it to be worked for 45
-//     int climbStairs(int n) {
-//         if(n<=0)
-//        {
-//         if(n==0)
-//             return 1;
-//         return 0;
-//        }
-//      return  climbStairs(n-1) + climbStairs(n-2);
-        
-//     }
 };
