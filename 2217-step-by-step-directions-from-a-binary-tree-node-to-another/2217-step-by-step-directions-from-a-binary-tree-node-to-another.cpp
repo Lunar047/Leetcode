fafbceb1e7e90e01474168e9d5bbae0e6e65c251
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-07-16 19:59:43 +0530

    Time: 152 ms (66.21%), Space: 119 MB (49.69%) - LeetHub

--- 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
+++ 2217-step-by-step-directions-from-a-binary-tree-node-to-another/2217-step-by-step-directions-from-a-binary-tree-node-to-another.cpp
@@ -14,52 +14,64 @@ private:
     string ans;
     map<int,TreeNode*> parent;
     set<int> vis;
-    void dfs(TreeNode*root,TreeNode*&start,TreeNode*&end,int &sv,int &ev){
-        if(!root)return;
-        // vis[root] = 0;
-        if(root->val==sv)start = root;
-        else if(root->val==ev)end = root;
-        if(root->left){
-            parent[root->left->val] = root;
-            dfs(root->left,start,end,sv,ev);
-        }
-        if(root->right){
-            parent[root->right->val] = root;
-            dfs(root->right,start,end,sv,ev);
-        }
+    // void dfs(TreeNode*root,TreeNode*&start,TreeNode*&end,int &sv,int &ev){
+    //     if(!root)return;
+    //     // vis[root] = 0;
+    //     if(root->val==sv)start = root;
+    //     else if(root->val==ev)end = root;
+    //     if(root->left){
+    //         parent[root->left->val] = root;
+    //         dfs(root->left,start,end,sv,ev);
+    //     }
+    //     if(root->right){
+    //         parent[root->right->val] = root;
+    //         dfs(root->right,start,end,sv,ev);
+    //     }
+    // }
+    TreeNode*LCA(TreeNode *root,int &start,int &end){
+        if(!root)return nullptr;
+        if(root->val==start || root->val==end)return root;
+        TreeNode* left=nullptr,*right = nullptr;
+        left = LCA(root->left,start,end);
+        right = LCA(root->right,start,end);
+        if(left && right)return root;
+        else if(!left)return right;
+        else return left;
     }
-    void bfs(TreeNode *root,int end,string &ans,string &cur){
+    void LCA_to_start(TreeNode *root,int &start,string &s,string &ans){
         if(!root)return;
-        // vis[root] =1;
-        vis.insert(root->val);
-        if(root->val==end){
-            ans = cur;
+        else if(root->val==start){
+            ans += s;
             return;
         }
-        if(root->left && !vis.count(root->left->val)){
-            cur.push_back('L');
-            bfs(root->left,end,ans,cur);
-            cur.pop_back();
+        s.push_back('U');
+        LCA_to_start(root->left,start,s,ans);
+        LCA_to_start(root->right,start,s,ans);
+        s.pop_back();
+        
+    }
+    void LCA_to_end(TreeNode *root,int &end,string &cur,string &ans){
+        if(!root)return;
+        else if(root->val==end){
+            ans+=cur;
+            return;
         }
-        if(root->right && !vis.count(root->right->val)){
-            cur.push_back('R');
-            bfs(root->right,end,ans,cur);
-            cur.pop_back();
-        }
-        if(parent[root->val] && !vis.count(parent[root->val]->val)){
-            cur.push_back('U');
-            bfs(parent[root->val],end,ans,cur);
-            cur.pop_back();
-        }
+        cur.push_back('L');
+        LCA_to_end(root->left,end,cur,ans);
+        cur.pop_back();
+        cur.push_back('R');
+        LCA_to_end(root->right,end,cur,ans);
+        cur.pop_back();
     }
 public:
     string getDirections(TreeNode* root, int startValue, int destValue) {
-        TreeNode*start,*end;
-        parent[root->val] = nullptr;
-        // vis[nullptr] = 1;
-        dfs(root,start,end,startValue,destValue);
-        string ans,cur;
-        bfs(start,destValue,ans,cur);
+        TreeNode *lca = LCA(root,startValue,destValue);
+        string cur,ans;
+        // cout<<lca->val<<" ";
+        LCA_to_start(lca,startValue,cur,ans);
+        // cout<<cur<<" "<<ans<<endl;
+        // cur.clear();
+        LCA_to_end(lca,destValue,cur,ans);
         return ans;
     }
 };
