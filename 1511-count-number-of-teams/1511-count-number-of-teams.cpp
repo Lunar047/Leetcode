520e1750f8a77b5cec7077bbe61c4f818a3035c2
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-04 15:07:42 +0530

    Time: 149 ms (26.77%), Space: 14.3 MB (24.71%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,13 +1,38 @@
 class Solution {
+private:
+    int n;
+    int solve(int ind,vector<int> &rating,vector<vector<int>> &dp,int size){
+        if(ind>=n)return 0;
+        if(size==2)return dp[ind][size] = 1;
+        if(dp[ind][size]!=-1)return dp[ind][size];
+        int ans = 0;
+        for(int i = ind+1;i<n;i++){
+            if(rating[i]>rating[ind]){
+                ans+=solve(i,rating,dp,size+1);
+            }
+        }
+        return dp[ind][size] = ans;
+    }
+    int solve2(int ind,vector<int> &rating,vector<vector<int>> &dp,int size){
+        if(ind>=n)return 0;
+        if(size==2)return   dp[ind][size] = 1;
+        if(dp[ind][size]!=-1)return dp[ind][size];
+        int ans = 0;
+        for(int i = ind+1;i<n;i++){
+            if(rating[i]<rating[ind]){
+                ans+=solve2(i,rating,dp,size+1);
+            }
+        }
+        return dp[ind][size] = ans;
+    }
 public:
     int numTeams(vector<int>& rating) {
-        int n = rating.size();
+        n = rating.size();
         int ans = 0;
-        for(int i=1;i<n-1;i++){
-            int l1 =0,r1=0,l2=0,r2=0;
-            for(int j=0;j<i;j++)if(rating[j]<rating[i])l1++;else if(rating[j]>rating[i])l2++;
-            for(int k=i+1;k<n;k++)if(rating[k]>rating[i])r1++;else if(rating[k]<rating[i])r2++;
-            ans+=(l1*r1 + l2*r2);
+        vector<vector<int>> dp(n,vector<int>(3,-1)),dp2 = dp;
+        for(int i=0;i<n;i++){
+            ans+=solve(i,rating,dp,0);
+            ans+=solve2(i,rating,dp2,0);
         }
         return ans;
     }
