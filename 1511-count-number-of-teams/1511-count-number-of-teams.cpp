b4b169d684323ed14981111e65ea3c1505a64d4b
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-04 15:11:11 +0530

    Time: 157 ms (26.73%), Space: 15.1 MB (17.67%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,38 +1,33 @@
 class Solution {
 private:
     int n;
-    int solve(int ind,vector<int> &rating,vector<vector<int>> &dp,int size){
+    int solve(int ind,vector<int> &rating,vector<vector<vector<int>>> &dp,int size,bool f){
         if(ind>=n)return 0;
-        if(size==2)return dp[ind][size] = 1;
-        if(dp[ind][size]!=-1)return dp[ind][size];
+        if(size==2)return dp[ind][f][size] = 1;
+        if(dp[ind][f][size]!=-1)return dp[ind][f][size];
         int ans = 0;
+        if(f)
         for(int i = ind+1;i<n;i++){
             if(rating[i]>rating[ind]){
-                ans+=solve(i,rating,dp,size+1);
+                ans+=solve(i,rating,dp,size+1,f);
             }
         }
-        return dp[ind][size] = ans;
-    }
-    int solve2(int ind,vector<int> &rating,vector<vector<int>> &dp,int size){
-        if(ind>=n)return 0;
-        if(size==2)return   dp[ind][size] = 1;
-        if(dp[ind][size]!=-1)return dp[ind][size];
-        int ans = 0;
+        else
         for(int i = ind+1;i<n;i++){
             if(rating[i]<rating[ind]){
-                ans+=solve2(i,rating,dp,size+1);
+                ans+=solve(i,rating,dp,size+1,f);
             }
         }
-        return dp[ind][size] = ans;
+        return dp[ind][f][size] = ans;
     }
 public:
     int numTeams(vector<int>& rating) {
         n = rating.size();
         int ans = 0;
-        vector<vector<int>> dp(n,vector<int>(3,-1)),dp2 = dp;
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
         for(int i=0;i<n;i++){
-            ans+=solve(i,rating,dp,0);
-            ans+=solve2(i,rating,dp2,0);
+            ans+=solve(i,rating,dp,0,1);
+            ans+=solve(i,rating,dp,0,0);
         }
         return ans;
     }
