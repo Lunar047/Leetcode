89f3d3b4a2d7cd95c1816b75c939dc5ede9324ca
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-05 20:42:20 +0530

    Time: 1019 ms (16.4%), Space: 497.2 MB (9.21%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -1,19 +1,49 @@
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
 class Solution {
 private:
     //for PBDS uncomment these lines
-    typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+    // typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+    vector<int> count;
+    typedef pair<int,int> pii;
+    void mergeSort(int left,int right,vector<pii> &nums){
+        if(left<right){
+        int mid = left + (right-left)/2;
+        mergeSort(left,mid,nums);
+        mergeSort(mid+1,right,nums);
+        merge(left,mid,right,nums);
+        }
+    }
+    void merge(int left,int mid,int right,vector<pii>&nums){
+        vector<pii> temp;
+        int i = left,j = mid+1;
+        while(i<=mid && j<=right){
+            if(nums[i].first>nums[j].first){
+                count[nums[i].second]+=(right-j+1);
+                temp.push_back(nums[i++]);
+            }
+            else temp.push_back(nums[j++]);
+        }
+        while(i<=mid)temp.push_back(nums[i++]);
+        while(j<=right)temp.push_back(nums[j++]);
+        for(int i=left;i<=right;i++)nums[i] = temp[i-left];
+    }
 public:
     vector<int> countSmaller(vector<int>& nums) {
         int n = nums.size();
-        vector<int> ans(n,0);
-        pbds A;
-        for(int i= n-1;i>=0;i--){
-            A.insert(nums[i]);
-            ans[i] = A.order_of_key(nums[i]);;
+        count.assign(n,0);
+        // pbds A;
+        vector<pii> temp;
+        for(int i=0;i<n;i++){
+            temp.push_back({nums[i],i});
         }
-        return ans;
+        mergeSort(0,n-1,temp);
+        // for(auto &i:temp)cout<<i.first<<" ";
+        // for(int i= n-1;i>=0;i--){
+            // A.insert(nums[i]);
+            // ans[i] = A.order_of_key(nums[i]);
+        // }
+        return count;
     }
 };
