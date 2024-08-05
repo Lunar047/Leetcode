1f76f633e3f34b486ee61293cee196043fe1ca1f
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-05 11:38:11 +0530

    Time: 637 ms (33.79%), Space: 168.4 MB (55.87%) - LeetHub

---
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -0,0 +1,22 @@
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+class Solution {
+private:
+    //for PBDS uncomment these lines
+
+    typedef tree<long long int, null_type, less_equal<long long int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,0);
+        pbds A;
+        for(int i= n-1;i>=0;i--){
+            A.insert(nums[i]);
+            int k = A.order_of_key(nums[i]);
+            // cout<<k<<" ";
+            ans[i] = k;
+        }
+        return ans;
+    }
+};
