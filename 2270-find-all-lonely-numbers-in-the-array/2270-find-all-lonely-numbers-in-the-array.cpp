b88a61a62e532eed5028da1f5e76fac68e3d0dbf
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-06 12:13:11 +0530

    Time: 600 ms (14.33%), Space: 175.4 MB (29.35%) - LeetHub

---
+++ 2270-find-all-lonely-numbers-in-the-array/2270-find-all-lonely-numbers-in-the-array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findLonely(vector<int>& nums) {
+        vector<int> ans;
+        map<int,int> mp;
+        for(auto &i:nums){
+            mp[i]++;
+        }
+        for(auto &i:nums){
+            if(mp.count(i+1) || mp.count(i-1) || mp[i]>1)continue;
+            else ans.push_back(i);
+        }
+        return ans;
+    }
+};
