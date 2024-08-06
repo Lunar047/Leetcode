0de8db85c67b992926209e57e5314f53188b71fb
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-06 12:09:56 +0530

    Time: 8 ms (92.97%), Space: 21.3 MB (5.44%) - LeetHub

--- 1511-count-number-of-teams/1511-count-number-of-teams.cpp
+++ 1511-count-number-of-teams/1511-count-number-of-teams.cpp
@@ -1,33 +1,38 @@
 class Solution {
 private:
-    int n;
-    int solve(int ind,vector<int> &rating,vector<vector<vector<int>>> &dp,int size,bool f){
-        if(ind>=n)return 0;
-        if(size==2)return dp[ind][f][size] = 1;
-        if(dp[ind][f][size]!=-1)return dp[ind][f][size];
-        int ans = 0;
-        if(f)
-        for(int i = ind+1;i<n;i++){
-            if(rating[i]>rating[ind]){
-                ans+=solve(i,rating,dp,size+1,f);
-            }
+    int n,mx;
+    vector<int> fen1,fen2;
+    void update(int idx,vector<int>&fen,bool i){
+        for(idx;idx<=mx;idx+=idx&-idx){
+            if(i)fen[idx]++;
+            else fen[idx]--;
         }
-        else
-        for(int i = ind+1;i<n;i++){
-            if(rating[i]<rating[ind]){
-                ans+=solve(i,rating,dp,size+1,f);
-            }
+    }
+    int sum(int idx,vector<int>&fen){
+        int res = 0;
+        for(idx;idx>0;idx-=idx&-idx){
+            res+=fen[idx];
         }
-        return dp[ind][f][size] = ans;
+        return res;
     }
 public:
     int numTeams(vector<int>& rating) {
         n = rating.size();
+        mx = *max_element(rating.begin(),rating.end());
+        fen1.assign(mx+10,0);
+        fen2.assign(mx+10,0);
+        for(int i=n-1;i>=0;i--){
+            update(rating[i],fen2,1);
+        }
         int ans = 0;
-        vector<vector<vector<int>>> dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
         for(int i=0;i<n;i++){
-            ans+=solve(i,rating,dp,0,1);
-            ans+=solve(i,rating,dp,0,0);
+           update(rating[i],fen2,0);
+           int left_small = sum(rating[i]-1,fen1);
+           int right_small = sum(rating[i]-1,fen2);
+           int left_greater = sum(mx,fen1) - sum(rating[i]-1,fen1);
+           int right_greater = sum(mx,fen2)-sum(rating[i]-1,fen2);
+           update(rating[i],fen1,1);
+            ans += (left_small*right_greater + left_greater*right_small);
         }
         return ans;
     }
