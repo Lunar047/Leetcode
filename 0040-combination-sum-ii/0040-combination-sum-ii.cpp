2089be013790ca19ed53bbac02c23adf454525b4
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-13 11:45:30 +0530

    Time: 3 ms (77.81%), Space: 13 MB (34.6%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,52 +1,27 @@
 class Solution {
 private:
-    vector<vector<int>> ans;
-    // set<vector<int>> st;
     int n;
-    // void solve(int i,vector<int>& candidates, int &target,vector<int>&cur,int sum){
-    //     if(i>=n){
-    //         if(sum==0){
-    //             // vector<int> t = cur;
-    //             // sort(t.begin(),t.end());
-    //             st.insert(cur);
-    //             // ans.push_back(cur);
-    //         }
-    //         return;
-    //     }
-    //     solve(i+1,candidates,target,cur,sum);
-    //     if(candidates[i]<=sum){
-    //     cur.push_back(candidates[i]);
-    //     solve(i+1,candidates,target,cur,sum-candidates[i]);
-    //     cur.pop_back();
-    //     }
-    // }
-    void solve(int i,vector<int>& candidates, int target,vector<int>&cur){
-        if(target==0){
-                ans.push_back(cur);
-                return;
+    vector<vector<int>> ans;
+    void solve(int idx,int target,vector<int>&candidates,vector<int>&cur){
+        if(!target){
+            ans.push_back(cur);
+            return;
         }
-        if(i>=n || candidates[i]>target)return;
-        for(int id = i;id<candidates.size();id++){
-            if(id^i && !(candidates[id]^candidates[id-1])){
-                continue;
-            }
-            else
-            {
-                // m[candidates[id]]++;
-                cur.push_back(candidates[id]);
-                solve(id+1,candidates,target-candidates[id],cur);
-                cur.pop_back();
-            }
+        if(target<0 || idx>=n)return;
+        for(int i= idx;i<n;i++){
+            if(idx^i && !(candidates[i]^candidates[i-1]))continue;
+            cur.push_back(candidates[i]);
+            solve(i+1,target-candidates[i],candidates,cur);
+            cur.pop_back();
         }
     }
+
 public:
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-       vector<int> cur;
-       n = candidates.size();
-       sort(candidates.begin(),candidates.end());
-    //    solve(0,candidates,target,cur,target);
-        solve(0,candidates,target,cur);
-    //    return {st.begin(),st.end()};
+        n = candidates.size();
+        sort(candidates.begin(),candidates.end());
+        vector<int> cur;
+        solve(0,target,candidates,cur);
         return ans;
     }
 };
