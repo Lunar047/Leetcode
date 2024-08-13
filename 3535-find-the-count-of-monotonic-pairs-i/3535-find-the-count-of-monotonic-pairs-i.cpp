89502b303a18d9f41c22fbdb6c251c99985e4110
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-13 18:59:09 +0530

    Time: 565 ms (55.3%), Space: 355.9 MB (45.99%) - LeetHub

--- 3535-find-the-count-of-monotonic-pairs-i/3535-find-the-count-of-monotonic-pairs-i.cpp
+++ 3535-find-the-count-of-monotonic-pairs-i/3535-find-the-count-of-monotonic-pairs-i.cpp
@@ -3,46 +3,10 @@ private:
     typedef long long int ll;
     ll mod = 1e9+7;
     int n;
-    ll solve(int i,int prev1,int prev2,vector<int>&nums,vector<vector<vector<ll>>> &dp){
-        if(i>=n)return 1;
-        if(dp[i][prev1][prev2] !=-1) return dp[i][prev1][prev2];
-        ll ans = 0;
-        for(int idx = prev1;idx<=nums[i];idx++){
-            int a2 = nums[i] - idx;
-            if(a2>=0 && a2<=prev2){
-                ans += (solve(i+1,idx,a2,nums,dp)%mod);
-            }
-        }
-        return dp[i][prev1][prev2] = ans%mod;
-    }
-     ll solve2(int i,int prev1,int prev2,vector<int>&nums,vector<vector<ll>> &dp){
-        if(i>=n)return 1;
-        if(dp[i][prev1] !=-1) return dp[i][prev1];
-        ll ans = 0;
-        for(int idx = prev1;idx<=nums[i];idx++){
-            int a2 = nums[i] - idx;
-            if(a2>=0 && a2<=prev2){
-                ans += (solve2(i+1,idx,a2,nums,dp)%mod);
-            }
-        }
-        return dp[i][prev1] = ans%mod;
-    }
-    // void solve3(int i,int prev1,int prev2,vector<int> &nums,vector<vector<ll>> &dp){
-    //     if(i>=n)return;
-    //     for(int idx = prev1;idx<=nums[i];idx++){
-    //         int a2 = nums[i] - idx;
-    //         if(a2>=0 && a2<=prev2){
-    //             dp[i][idx] += dp[i-1][]
-    //         }
-    //     }
-    // }
 public:
     int countOfPairs(vector<int>& nums) {
         n = nums.size();
-        // vector<vector<vector<ll>>> dp(1002,vector<vector<ll>>(52,vector<ll>(52,0)));
         vector<vector<ll>> dp(2001,vector<ll>(52,0));
-        // return solve2(0,0,nums[0],nums,dp2)%mod;
-        // return solve(0,0,nums[0],nums,dp)%mod;
         for(int i=0;i<=nums[0];i++){
            dp[0][i] = 1;
         }
@@ -58,9 +22,6 @@ public:
                 }
             }
         }
-        // for(int i= 0;i<n;i++){
-        //     for(int j = 0;j<=nums[i];j++)cout<<dp[i][j]<<" ";cout<<endl;
-        // }
         ll ans = 0;
         for(int i = 0;i<=nums[n-1];i++){
             ans += dp[n-1][i];
