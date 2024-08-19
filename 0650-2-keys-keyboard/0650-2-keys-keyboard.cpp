cd00a1a95376cda7273dfebb850d333f9416f87e
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-19 20:56:08 +0530

    Time: 12 ms (42.9%), Space: 8.2 MB (38.21%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -3,9 +3,8 @@ private:
     typedef long long ll;
 public:
     int minSteps(int n) {
-        vector<int> dp(1004,0);
-        dp[2] = 2;
-        for(int i=3;i<=n;i++) dp[i] = 1 + dp[i-1];
+        vector<int> dp(n+1,1000);
+        dp[1] = 0;
         // for(int i=0;i<=n;i++)cout<<dp[i]<<" ";
         for(int i=2;i<=n;i++){
             for(int j = 1;j<=i/2;j++){
