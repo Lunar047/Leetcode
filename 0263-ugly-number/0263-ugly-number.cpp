5d6e7ac87627749bfaf362a2ad2b041ba10aab8d
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-19 11:45:47 +0530

    Time: 6 ms (24.02%), Space: 7.8 MB (9.78%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    typedef long long ll;
+    vector<ll> factors(ll n){
+        vector<ll> fac;
+        ll i = 2;
+        for(i;i*i<=n;i++){
+            if(n%i==0){
+                fac.push_back(i);
+                // if(n/i != i)fac.push_back(n/i);
+                while(n%i==0)n/=i;
+            }
+        }
+        if(n>1)fac.push_back(n);
+        return fac;
+    }
+public:
+    bool isUgly(int n) {
+        if(n<=0)return 0;
+        vector<ll> f = factors(n);
+        for(auto &i:f){
+            cout<<i<<" ";
+            if(i==2 || i==3 || i==5)continue;
+            else return 0;
+        }
+        return 1;
+    }
+};
