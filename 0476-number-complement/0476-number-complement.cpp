67319681e2f52fe18e54f3c24508e7c1390a132a
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-22 20:35:17 +0530

    Time: 3 ms (41.07%), Space: 7.5 MB (57.13%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        for(int i=0;i<31 && num>=(1<<i);i++){
+            if(num&(1<<i))num &= (~(1<<i));
+            else num |= (1<<i);
+        }
+        return num;
+    }
+};
