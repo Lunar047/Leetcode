6880dfc577f445c5479b11b0ac3f6109427626e3
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-08-28 12:53:09 +0530

    Time: 452 ms (41.97%), Space: 11.8 MB (43.19%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -4,13 +4,14 @@ public:
         int ans = 0;
         vector<bool> is_prime(n+2, true);
         is_prime[0] = is_prime[1] = false;
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i*i <= n; i++) {
             if (is_prime[i]) {
-                ans++;
+                // ans++;
                 for (int j = i * 2; j <=n; j += i)
                     is_prime[j] = false;
             }
         }
+        for(int i=0;i<n;i++)if(is_prime[i])ans++;
         return ans;
     }
 };
