b3940afc3efc3a4b04331b981c4b40e841d6a9f1
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-09-04 23:01:57 +0530

    Time: 78 ms (57.73%), Space: 37.3 MB (83.7%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -3,24 +3,17 @@ private:
     map<pair<int,int>,bool> mp; 
     int n;
     int ans = 0;
-    void rotate_right(int &x,int &y){
-        int x_n = 0*x + y * 1,y_n = -1*x + 0*y;
-        x = x_n;
-        y = y_n;
-    }
-    void rotate_left(int &x,int &y){
-        int x_n = 0*x + y * -1,y_n = 1*x + 0*y;
-        x = x_n;
-        y = y_n;
-    }
     void solve(int i,int &x,int &y,int dx,int dy,vector<int>& commands){
-        cout<<n<<endl;
         for(i=0;i<n;i++){
             if(commands[i]==-2){
-                rotate_left(dx,dy);
+                    int x_n = 0*dx + dy * -1,y_n = 1*dx + 0*dy;
+                    dx = x_n;
+                    dy = y_n;
             }
             else if(commands[i]==-1){
-                rotate_right(dx,dy);
+               int x_n = 0*dx + dy * 1,y_n = -1*dx + 0*dy;
+                dx = x_n;
+                dy = y_n;
             }
             else{
                 for(int j=0;j<commands[i];j++){
