5dc930b5e82661666f4072c3a3520a2ec4e491bd
Sandeep saurav <94745770+Lunar047@users.noreply.github.com>
2024-09-09 21:05:22 +0530

    Time: 11 ms (27.37%), Space: 27 MB (7.78%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -2,19 +2,19 @@ class Solution {
 private:
     typedef long long ll;
     int n;
-    ll solve(int i,int j,bool f,vector<int>&p,vector<vector<vector<ll>>>&dp){
-        if(i>=j)return 0;
+    ll solve(int i,int j,bool f,vector<int>&p,vector<vector<ll>>&dp){
+        if(i>j)return 0;
         // cout<<f<<" "<<i<<" "<<j<<endl;
-        if(dp[f][i][j]!=-1)return dp[f][i][j];
+        if(dp[i][j]!=-1)return dp[i][j];
         if(f){
-            return  dp[f][i][j] = max(p[i] + solve(i+1,j,!f,p,dp) , p[j] + solve(i,j-1,!f,p,dp));
+            return  dp[i][j] = max(p[i] + solve(i+1,j,!f,p,dp) , p[j] + solve(i,j-1,!f,p,dp));
         }
-        else return dp[f][i][j] = max(solve(i+1,j,!f,p,dp) , solve(i,j-1,!f,p,dp));
+        else return dp[i][j] = max(solve(i+1,j,!f,p,dp) , solve(i,j-1,!f,p,dp));
     }
 public:
     bool stoneGame(vector<int>& piles) {
         int i=0,j = piles.size()-1;
-        vector<vector<vector<ll>>> dp(2,vector<vector<ll>>(j+1,vector<ll>(j+1,-1)));
+        vector<vector<ll>> dp(j+1,vector<ll>(j+1,-1));
         bool f = 1;
         ll alice = solve(0,j,1,piles,dp);
         ll total = accumulate(piles.begin(),piles.end(),0);
